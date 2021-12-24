d668ad7a61ec005057c8afb9cbfdcff3fe21c045
murilo <muriraubach@gmail.com>
2021-12-24 12:24:09 -0300

    feat: add navigation between home page and new room page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom'
+
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg';
@@ -7,6 +9,12 @@ import Button from '../components/Button';
 import '../styles/auth.scss';
 
 const Home = () => {
+  const navigate = useNavigate()
+
+  function handleCreateRoom() {
+    navigate('/rooms/new')
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -17,7 +25,7 @@ const Home = () => {
       <main>
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
-          <button className="create-room">
+          <button onClick={handleCreateRoom} className="create-room">
             <img src={googleIconImg} alt="Logo do google" />
             Crie sua sala com o Google
           </button>
