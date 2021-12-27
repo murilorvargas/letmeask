6d4d8bdcec2307b4c9dcc8d4f1fb721b4a8cb280
murilo <muriraubach@gmail.com>
2021-12-26 22:40:47 -0300

    feat: add the flow to create a room on the new room page

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,13 +1,39 @@
 import { Link } from 'react-router-dom'
+import { FormEvent, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import { database } from '../services/firebase';
+import useAuth from '../hooks/useAuth';
+
+import Button from '../components/Button';
 
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 
-import Button from '../components/Button';
 
 import '../styles/auth.scss';
 
 const NewRoom = () => {
+  const { user } = useAuth()
+  const navigate = useNavigate()
+  const [newRoom, setNewRoom] = useState('')
+
+  async function handleCreateRoom(event: FormEvent) {
+    event.preventDefault()
+    if (newRoom.trim() === '') {
+      return;
+    }
+
+    const roomRef = database.ref('rooms')
+
+    const firebaseRoom = await roomRef.push({
+      title: newRoom,
+      authorId: user?.id,
+    })
+
+    navigate(`/rooms/${firebaseRoom.key}`)
+  }
+
   return (
     <div id="page-auth">
       <aside>
@@ -19,8 +45,8 @@ const NewRoom = () => {
         <div className="main-content">
           <img src={logoImg} alt="Letmeask" />
           <h2>Criar uma nova sala</h2>
-          <form>
-            <input type="text" placeholder="Nome da sala" />
+          <form onSubmit={handleCreateRoom}>
+            <input type="text" placeholder="Nome da sala" onChange={event => setNewRoom(event.target.value)} value={newRoom} />
             <Button type="submit">Criar sala</Button>
           </form>
           <p>
